a3981527669b0d8c774ff0bdfea2f7f25cf74e47
yeganer <yeganer@gmx.de>
2017-08-31 12:18:01 +0200

    Delete miniconda so conda can create it

--- travis/setup_conda_linux.sh
+++ travis/setup_conda_linux.sh
@@ -6,8 +6,9 @@ echo ls -la $HOME/miniconda
 ls -la $HOME/miniconda
 if [ ! "$(ls -A $HOME/miniconda)" ]; then
   # Control will enter here if $HOME/miniconda is empty.
-    wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
-    bash miniconda.sh -b -p $HOME/miniconda
+  rm -r $HOME/miniconda
+  wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
+  bash miniconda.sh -b -p $HOME/miniconda
 fi
 
 
